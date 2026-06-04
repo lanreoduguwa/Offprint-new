1b18f97160826b92d9b71e31df23f6117f23f6f6
lanreoduguwa <lanreoduguwa58@gmail.com>
2026-06-04 13:32:07 +0100

    Fix adminroutes to import from authController

--- routes/adminroutes.js
+++ routes/adminroutes.js
@@ -1,7 +1,7 @@
 const express    = require("express");
 const router     = express.Router();
 const { protect }        = require("../middleware/authMiddleware");
-const { adminDashboard } = require("../controllers/adminController");
+const { adminDashboard } = require("../controllers/authController");
 
 router.get("/", protect, adminDashboard);
 
